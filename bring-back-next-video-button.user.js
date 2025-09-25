f850335dfb20d1170be3f1d3b4e8d9763cefdb13
Etshy <garbuio.jonas@gmail.com>
2025-09-25 21:28:36 +0200

    Update bring-back-next-video-button.user.js

--- bring-back-next-video-button.user.js
+++ bring-back-next-video-button.user.js
@@ -2,9 +2,9 @@
 // @name         Bring back next video button
 // @license      GNU GPLv3
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @author       Etshy
-// @match        *://*youtube.com*
+// @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
