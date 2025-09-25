642645439fe373ab7326c70263984dbc63161d99
Etshy <garbuio.jonas@gmail.com>
2025-09-25 21:18:15 +0200

    Update bring-back-next-video-button.user.js

--- bring-back-next-video-button.user.js
+++ bring-back-next-video-button.user.js
@@ -2,9 +2,9 @@
 // @name         Bring back next video button
 // @license      GNU GPLv3
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @author       Etshy
-// @match        *://www.youtube.com/watch*
+// @match        *://*youtube.com*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -12,14 +12,19 @@
 (function() {
     'use strict';
 
+    function showNextButton() {
+        waitForElm('.ytp-next-button').then(elm => {
+            elm.style.removeProperty("display");
+        });
+    }
+
+    // A helper function to wait for a specific element to exist in the DOM.
     function waitForElm(selector) {
         return new Promise(resolve => {
-
             //If the element already exists, resolve the Promise
             if (document.querySelector(selector)) {
                 return resolve(document.querySelector(selector));
             }
-
             //Or observe document.body
             const observer = new MutationObserver(mutations => {
                 //as soon as the element exists, resolve the Promise
@@ -28,7 +33,6 @@
                     resolve(document.querySelector(selector));
                 }
             });
-
             observer.observe(document.body, {
                 childList: true,
                 subtree: true
@@ -36,7 +40,22 @@
         });
     }
 
-    waitForElm('.ytp-next-button').then(elm => {
-        elm.style.removeProperty("display");
+    // Initial run of the script.
+    showNextButton();
+
+    // Listen for URL changes and re-run the script.
+    let lastUrl = location.href;
+    const urlObserver = new MutationObserver(() => {
+        const currentUrl = location.href;
+        if (currentUrl !== lastUrl) {
+            lastUrl = currentUrl;
+            showNextButton();
+        }
+    });
+
+    // Observe the body for changes in the subtree, which indicates a new page load.
+    urlObserver.observe(document, {
+        childList: true,
+        subtree: true
     });
 })();
